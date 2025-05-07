f1d9b4732ce67d31e83e42b0d2aacadb13bed207
falconbird18 <austintallent25@outlook.com>
2025-05-06 17:06:48 -0700

    Add app bar.

--- .config/ags/widget/TaskBar/index.tsx
+++ .config/ags/widget/TaskBar/index.tsx
@@ -1,18 +1,39 @@
 import { Astal, App, Gtk, Gdk } from "astal/gtk3";
 import { spacing } from "../../lib/variables";
+import { bind } from "astal";
+import Hyprland from "gi://AstalHyprland";
+import icons, { substitutions } from "../../lib/icons";
+import { lookUpIcon } from "../../lib/utils";
 
-// declare global {
-//   namespace JSX {
-//     interface IntrinsicElements {
-//       box: ConstructProps<Gtk.Box, Gtk.Box.ConstructorProps> & Ref<Gtk.Box>;
-//       window: ConstructProps<Widget.Window, Widget.Window.ConstructorProps> & Ref<Widget.Window>;
-//       centerbox: ConstructProps<Gtk.Box, Gtk.Box.ConstructorProps> & Ref<Gtk.Box>;
-//     }
-//   }
-// }
+const TaskBarItem = ({ client }: { client: any }) => {
+    const icon = substitutions.icons[client.class]
+        ? substitutions.icons[client.class]
+        : lookUpIcon(client.class)
+            ? client.class
+            : icons.fallback.executable;
 
+    return (
+        <button
+            className="taskbar-item"
+            tooltipText={client.title}
+            onClicked={() => {
+                if (client.address) {
+                    Hyprland.get_default().dispatch(`focuswindow address:${client.address}`);
+                }
+            }}
+        >
+            <box spacing={spacing}>
+                <icon icon={icon} />
+                <label label={client.title} visible={false} />
+            </box>
+        </button>
+    );
+};
 
 export default function TaskBar(gdkmonitor: Gdk.Monitor) {
+    const hyprland = Hyprland.get_default();
+    const clients = bind(hyprland, "clients");
+
     return (
         <window
             vexpand={true}
@@ -25,10 +46,21 @@ export default function TaskBar(gdkmonitor: Gdk.Monitor) {
                 Astal.WindowAnchor.BOTTOM |
                 Astal.WindowAnchor.LEFT |
                 Astal.WindowAnchor.RIGHT
-              }
+            }
             application={App}
         >
             <centerbox className="bar" valign={Gtk.Align.CENTER}>
+                <box halign={Gtk.Align.START} />
+                <box spacing={spacing} halign={Gtk.Align.CENTER}>
+                    {clients.as(clients => 
+                        clients
+                            .filter(client => !client.class.includes("unmanaged"))
+                            .map(client => (
+                                <TaskBarItem key={client.address} client={client} />
+                            ))
+                    )}
+                </box>
+                <box halign={Gtk.Align.END} />
             </centerbox>
         </window>
     );
